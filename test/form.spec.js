5c193023da43e5e530b08edc2e20de81109e38e4
andy23512 <andy23512@gmail.com>
2018-02-15 11:45:03 +0800

    add test folder, init form.spec.js

---
+++ test/form.spec.js
@@ -0,0 +1,24 @@
+import faker from "faker"
+import puppeteer from "puppeteer"
+import fs from "fs"
+
+// port and host of dev server
+const port = parseInt(fs.read-file-sync('../option/port', {encoding: 'utf8'}))
+const host = fs.read-file-sync('../option/host', {encoding: 'utf8'}).replace('\n', '')
+
+// form url
+const APP = `http://${host}:${port}`
+
+// fake user
+const lead = {
+  name: faker.name.firstName(),
+  email: faker.internet.email(),
+  phone: faker.phone.phoneNumber(),
+  message: faker.random.words()
+}
+
+// variables for puppeteer
+let page
+let browser
+const width = 1920
+const height = 1080
