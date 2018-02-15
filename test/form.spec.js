51c26d88e03162605d30b507f71ea77b1056bd7e
andy23512 <andy23512@gmail.com>
2018-02-15 14:25:30 +0800

    fix bug in form.spec.js, make sure test can work

--- test/form.spec.js
+++ test/form.spec.js
@@ -3,8 +3,8 @@ import puppeteer from "puppeteer"
 import fs from "fs"
 
 // port and host of dev server
-const port = parseInt(fs.read-file-sync('../option/port', {encoding: 'utf8'}))
-const host = fs.read-file-sync('../option/host', {encoding: 'utf8'}).replace('\n', '')
+const port = parseInt(fs.readFileSync('option/port', {encoding: 'utf8'}))
+const host = fs.readFileSync('option/host', {encoding: 'utf8'}).replace('\n', '')
 
 // form url
 const APP = `http://${host}:${port}`
@@ -40,17 +40,17 @@ afterAll(() => {
 describe("Contact form", () => {
   test("lead can submit a contact request", async () => {
     await page.goto(APP)
-    await page.waitForSelector('[data-test=contact-from]')
+    await page.waitForSelector('[data-test=contact-form]')
     await page.click('input[name=name]')
     await page.type('input[name=name]', lead.name)
     await page.click('input[name=email]')
     await page.type('input[name=email]', lead.email)
     await page.click('input[name=tel]')
-    await page.type('input[name=tel]', lead.tel)
+    await page.type('input[name=tel]', lead.phone)
     await page.click('textarea[name=message]')
     await page.type('textarea[name=message]', lead.message)
     await page.click('input[type=checkbox]')
     await page.click('button[type=submit]')
     await page.waitForSelector('.modal', {visible: true})
-  }, 16000)
+  }, 20000)
 })
